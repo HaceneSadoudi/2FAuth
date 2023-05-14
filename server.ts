de252f13d27ca08daed3da2898b44147bd906042
HaceneSadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2023-05-14 20:47:56 +0200

    :bug: handle env port for deploy

--- server.ts
+++ server.ts
@@ -37,7 +37,7 @@ async function main() {
     });
   });
 
-  const PORT = 8000;
+  const PORT = process.env.PORT || 8000;
   app.listen(PORT, () => {
     console.info(`Server started on port: ${PORT}`);
   });
